396bd94b36e3ed2a7714755ecb858bb24e4d5f2c
amritghimire <amritghimire@users.noreply.github.com>
2023-07-13 02:25:53 +0000

    deploy: 382d303dff4c3155c74fd495c5937cbeccdb306e

--- personal-frontend-3e11613a25130fa5.js
+++ personal-frontend-3e11613a25130fa5.js
@@ -859,7 +859,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('personal-frontend-4d69b56984be9b8c_bg.wasm', import.meta.url);
+        input = new URL('personal-frontend-3e11613a25130fa5_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
